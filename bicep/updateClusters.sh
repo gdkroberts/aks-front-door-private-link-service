2d231da0aa63fa22f79d661aefbb8c2c5b63fb85
Graham Roberts <gdkroberts@hotmail.co.uk>
2024-11-05 22:29:20 +0000

    myChart

--- bicep/updateClusters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clusters=("MiaAks" "SallyAks" "EdwardAks" "KaluaAks" "BabosbirdAks" "SethAks")
-resourceGroupNames=("MiaRG" "SallyRG" "EdwardRG" "KaluaRG" "BabosbirdRG" "SethRG")
-
-for ((i = 0; i < ${#clusters[@]}; i++)); do
-  az aks update --resource-group ${resourceGroupNames[$i]} --name ${clusters[$i]} --enable-vpa
-done
